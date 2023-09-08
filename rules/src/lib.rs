12fb259f5fba374b7264e25fa860dff27ee42dab
Søren Alsbjerg Hørup <sah@sah.dk>
2023-09-08 20:14:58 +0200

    wip

---
+++ rules/src/lib.rs
@@ -0,0 +1,77 @@
+/*
+
+
+use core::{
+    glam::{IVec2, Vec2},
+    registry::EntityId,
+    Facade,
+};
+use std::collections::HashMap;
+
+#[derive(Clone, Copy)]
+pub struct ReachableCell {
+    pub to: IVec2,
+    pub cost_ft: i16,
+    pub from :IVec2
+}
+
+fn move_from(
+    f: &Facade,
+    pos: IVec2,
+    h: &mut HashMap<IVec2, ReachableCell>,
+    movement_total_ft: i16,
+    movement_cost_ft: i16,
+) {
+    let ds = [
+        IVec2::new(-1, -1),
+        IVec2::new(0, -1),
+        IVec2::new(1, -1),
+        IVec2::new(-1, 0),
+        IVec2::new(1, 0),
+        IVec2::new(-1, 1),
+        IVec2::new(0, 1),
+        IVec2::new(1, 1),
+    ];
+    for d in ds {
+        let new_pos = pos + d;
+        if let Some(cell) = f.map.get(new_pos.x as usize, new_pos.y as usize) {
+            if cell.blocked {
+                continue;
+            }
+        }
+
+        let movement_cost_ft = movement_cost_ft + 5;
+        if movement_cost_ft > movement_total_ft {
+            continue;
+        }
+
+        if let Some(e) = h.get_mut(&new_pos) {
+            if e.cost_ft > movement_cost_ft {
+                e.cost_ft = movement_cost_ft;
+                e.from = pos;
+                move_from(f, new_pos, h, movement_total_ft, movement_cost_ft);
+            }
+        } else {
+            h.insert(
+                new_pos,
+                ReachableCell {
+                    from:pos,
+                    to: new_pos,
+                    cost_ft: movement_cost_ft,
+                },
+            );
+            move_from(f, new_pos, h, movement_total_ft, movement_cost_ft);
+        }
+        //move_from(f, new_pos, h, movement_total_ft, movement_cost_ft);
+    }
+}
+
+pub fn get_reachable_cells(f: &Facade, entity: EntityId) -> HashMap<IVec2, ReachableCell> {
+    let mut vec = HashMap::new();
+    if let Some(token) = f.tokens.get(entity) {
+        move_from(f, token.pos, &mut vec, token.movement, 0);
+        vec.remove(&token.pos);
+    }
+    vec
+}
+*/
