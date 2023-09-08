36c86183cbd7731d88cb53b574cd60b95efe198b
= <horup@deepstacker.com>
2023-09-08 14:59:48 +0200

    cool

--- plugin_ui/src/lib.rs
+++ plugin_ui/src/lib.rs
@@ -4,43 +4,9 @@ use bevy::{
     prelude::*,
 };
 use common::{CommonAssets, Round, RoundCommand, Selection, Token, UIDebugFPS, WorldCursor, UI};
-fn system_ui_startup(mut commands: Commands, common_assets: ResMut<CommonAssets>) {
-    // spawn camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 32.0)
-            .looking_at(Vec3::new(0.0, 16.0, 0.0), Vec3::Y),
-        ..default()
-    });
 
-    let font = common_assets.font("default");
-    commands
-        .spawn(
-            TextBundle::from_section(
-                "---",
-                TextStyle {
-                    font: font.clone(),
-                    font_size: 16.0,
-                    color: Color::RED,
-                },
-            )
-            .with_style(Style {
-                position_type: PositionType::Absolute,
-                top: Val::Px(5.0),
-                left: Val::Px(5.0),
-                ..default()
-            }),
-        )
-        .insert(UIDebugFPS);
-
-    // spawn world cusor
-    commands
-        .spawn(PbrBundle {
-            mesh: common_assets.mesh("selector"),
-            material: common_assets.material("white"),
-            ..Default::default()
-        })
-        .insert(WorldCursor::default());
-}
+mod startup;
+pub use startup::*;
 
 fn update_camera(
     keys: Res<Input<KeyCode>>,
