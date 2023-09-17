7b6e6e80d7e57bf95e3a322e2d16d45665191975
= <horup@deepstacker.com>
2023-09-17 08:37:48 +0200

    assets

--- common/src/assets.rs
+++ common/src/assets.rs
@@ -1,6 +1,4 @@
-use std::io::BufReader;
-
-use bevy::{reflect::{TypeUuid, TypePath}, asset::AssetLoader, prelude::{App, AddAsset}};
+use bevy::{reflect::{TypeUuid, TypePath}, asset::{AssetLoader, LoadedAsset}, prelude::{App, AddAsset}};
 use serde::{Serialize, Deserialize};
 #[derive(TypeUuid, TypePath, Serialize, Deserialize)]
 #[uuid = "f175d5c6-4275-4e40-9105-016d4d0001c1"]
@@ -20,11 +18,11 @@ impl AssetLoader for StablockAssetLoader {
         Box::pin(async move {
             match serde_json::from_slice::<Statblock>(bytes) {
                 Ok(statblock) => {
-                    dbg!("ha");
+                    load_context.set_default_asset(LoadedAsset::new(statblock));
                     return Ok(());
                 },
                 Err(err) => {
-                    return Err(bevy::asset::Error::msg("failed to deserialize .statblock"));
+                    return Err(bevy::asset::Error::msg(err.to_string()));
                 },
             }
         })
@@ -36,5 +34,6 @@ impl AssetLoader for StablockAssetLoader {
 }
 
 pub fn build(app:&mut App) {
+    app.add_asset::<Statblock>();
     app.init_asset_loader::<StablockAssetLoader>();
 }
